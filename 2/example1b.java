67d83d72f6fb2059aea5bef5f17fc68cb3957a4d
Patrick Dolloso <patrick.dolloso@gmail.com>
2024-03-11 02:49:54 +0000

    commit

---
+++ 2/example1b.java
@@ -0,0 +1,9 @@
+public class example1 {
+    public static void main(String[] args) {
+        for (int i = 100; i >= 0; i--){
+            if (i % 2 == 0) {
+                System.out.print(i + " ");
+            }
+        }
+    }
+}
