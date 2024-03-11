c5313bc93e5d03e16b1a4d403f36db4e12755494
Patrick Dolloso <patrick.dolloso@gmail.com>
2024-03-11 02:16:28 +0000

    test

--- 2/test.java
+++ 2/test.java
@@ -1,1 +1,15 @@
-pu
+import java.util.Scanner;
+
+public class test {
+    int n;
+    while (true) {
+        System.out.print("Enter a number between 1 and 10: ");
+        n = s.nextInt();
+
+        if (n < 1 || n > 10) {
+            continue;
+        }
+        System.out.println(n + " is between 1 and 10.");
+        break;
+    }
+}
